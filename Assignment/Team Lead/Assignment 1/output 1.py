f18619fd4917bbefd05e75226e0ac3a4f90a9996
Snekagovindan <106261488+Snekagovindan@users.noreply.github.com>
2022-09-23 14:16:21 +0530

    Add files via upload

---
+++ Assignment/Team Lead/Assignment 1/output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:\Users\Hi\Downloads\coding 1.py ==================
+[1, 2, 3, 4, 5]
+[1, 2, 3, 4, 5, 10]
+[2, 3, 4, 5, 10]
+[2, 3, 4, 5, 10, -5]
+[-5, 2, 3, 4, 5, 10]
+[-5, 2, 3, 4, 5]
+[5, 4, 3, 2, -5]
+HelloWorld
+noitacudE
+dc
