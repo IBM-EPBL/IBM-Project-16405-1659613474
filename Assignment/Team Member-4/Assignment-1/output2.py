1ff81f3ce7aa057f4ba84db0d3037ae36014905f
Neha1224 <112812729+Neha1224@users.noreply.github.com>
2022-09-23 14:47:29 +0530

    Add files via upload

---
+++ Assignment/Team Member-4/Assignment-1/output2.py
@@ -0,0 +1,14 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================================================================== RESTART: C:\Users\91938\Downloads\coding 2.py ====================================================================
+please select operation -
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+
+select operations from 1, 2, 3, 4 :3
+Enter first number: 25
+Enter second number: 15
+25 * 15 = 375
