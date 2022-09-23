01c9c7833262ffefbffe933b3b072ee33a09c085
ssubasri1234 <112814108+ssubasri1234@users.noreply.github.com>
2022-09-23 16:03:36 +0530

    Add files via upload

---
+++ Assignment/Team Member-2/Assignment-1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[1,2,3,4,5,6,7,9,10]
+print(list)
+
+#insertion
+list.insert(2,6)
+print(list)
+
+#deletion of first occurence of integer
+del list[2]
+print(list)
+
+#insertion at end of list
+list.append(-3)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("GOOD")
+str2=("MORNING")
+print(str1+str2)
+
+#reverse of String
+str3="BAD"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,4,2)])
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignment/Team Member-2/Assignment-1/output 2.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: F:/Team Member-3/coding 2.py ====================
+please select operation -
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+
+select operations from 1, 2, 3, 4 :1
+Enter first number: 23
+Enter second number: 89
+23 + 89 = 112
