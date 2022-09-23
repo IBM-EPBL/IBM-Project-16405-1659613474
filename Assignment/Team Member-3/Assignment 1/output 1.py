dd2e2b862087368c4cc9d86aa5448f0d30d152c7
Sindhu36939 <112792358+Sindhu36939@users.noreply.github.com>
2022-09-23 15:55:21 +0530

    Add files via upload

---
+++ Assignment/Team Member-3/Assignment 1/output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:\Users\Hi\Downloads\coding 1.py ==================
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+[1, 2, 56, 3, 4, 5, 6, 7, 8, 9, 10]
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -3]
+[-3, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+[-3, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, -3]
+HIWELCOME
+DOOG
+OD
