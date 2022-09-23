bbad71ad6099613dab3934dc4c2c3e1233046022
Neha1224 <112812729+Neha1224@users.noreply.github.com>
2022-09-23 14:45:31 +0530

    Add files via upload

---
+++ Assignment/Team Member-4/Assignment-1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[1,2,3,4,5]
+print(list)
+
+#insertion
+list.insert(5,10)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(-5)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("good")
+str2=("Morning")
+print(str1+str2)
+
+#reverse of String
+str3="Nalaiyathiran"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,5,2)])
+
+
+
+
+
+
+
+
+
+
+
+
