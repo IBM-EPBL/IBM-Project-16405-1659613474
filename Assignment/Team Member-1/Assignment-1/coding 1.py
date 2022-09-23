f43319229fd8730522a9cd9549937d44cad3c510
Ragaviabi <113878930+Ragaviabi@users.noreply.github.com>
2022-09-23 15:19:47 +0530

    Add files via upload

---
+++ Assignment/Team Member-1/Assignment-1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[1,2,3,4,5,6,7]
+print(list)
+
+#insertion
+list.insert(5,13)
+print(list)
+
+#deletion of first occurence of integer
+del list[2]
+print(list)
+
+#insertion at end of list
+list.append(-5)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("MOM")
+str2=("DAD")
+print(str1+str2)
+
+#reverse of String
+str3="FAMILY"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,4,2)])
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignment/Team Member-1/Assignment-1/output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:\Users\Hi\Downloads\coding 1.py ==================
+[1, 2, 3, 4, 5, 6, 7]
+[1, 2, 3, 4, 5, 13, 6, 7]
+[1, 2, 4, 5, 13, 6, 7]
+[1, 2, 4, 5, 13, 6, 7, -5]
+[-5, 1, 2, 4, 5, 6, 7, 13]
+[-5, 1, 2, 4, 5, 6, 7]
+[7, 6, 5, 4, 2, 1, -5]
+MOMDAD
+YLIMAF
+AI
